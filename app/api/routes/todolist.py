27731084d3e6690c72a315c3bf8435f180128a26
Alexsandr Crist <pp89635075943@gmail.com>
2024-05-28 11:09:36 +0300

    add HTTP methods

--- app/api/routes/todolist.py
+++ app/api/routes/todolist.py
@@ -2,47 +2,61 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models.model import ToDoListOrm
 from schemas.schemas import ToDoListDTO, ToDoListPostDTO
-from db.database import sync_session_fabric
 from api.dependencies.database import get_session
-from services.todolist import select_todo_by_id
-
-
+from services.todolist import select_todo_by_id, insert_data_to_db, update_todo_with_put, delete_data_from_db
 
 
 router = APIRouter()
 
 
-@router.post('/new_todo/')
-async def post_create_new_todo(data: ToDoListPostDTO):
-    with sync_session_fabric() as session:
-        result = ToDoListOrm(**data.model_dump())
-        session.add(result)
-        session.commit()
-    return {'msg': 'Nice!'}
+@router.post("/new_todo/")
+async def post_create_new_todo(
+    session: Annotated[AsyncSession, Depends(get_session)], data: ToDoListPostDTO
+):
+    try:
+        await insert_data_to_db(session, data)
+        return {"msg": "Data insert"}
+    except:
+        return {"msg error": "Data not insert"}
 
 
-@router.get('/todo/{todo_id}/', response_model=ToDoListDTO)
-async def get_todo_by_id(todo_id: int, session: Annotated[AsyncSession, Depends(get_session)]):
+@router.get("/todo/{todo_id}/", response_model=ToDoListDTO)
+async def get_todo_by_id(
+    todo_id: int, session: Annotated[AsyncSession, Depends(get_session)]
+):
     result = await select_todo_by_id(todo_id, session)
 
     if result is None:
         raise HTTPException(
-            status.HTTP_404_NOT_FOUND,
-            detail='По указаному id нечиго не найдено [!]'
+            status.HTTP_404_NOT_FOUND, detail="По указаному id нечиго не найдено [!]"
         )
 
     return ToDoListDTO.model_validate(result, from_attributes=True)
 
 
-@router.put('/todo_update_put/{todo_id}/')
+@router.put("/todo_update_put/{todo_id}/")
 async def put_todo(
     todo_id: int,
-    todo: ToDoListPostDTO,
-    session: Annotated[AsyncSession, Depends(get_session)]
+    data: ToDoListPostDTO,
+    session: Annotated[AsyncSession, Depends(get_session)],
 ):
-    ...
+    if await select_todo_by_id(todo_id, session) is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, detail="По указаному id нечиго не найдено [!]"
+        )
+    try:
+        await update_todo_with_put(todo_id, session, data)
+        return {"msg": "Data update"}
+    except:
+        return {"msg error": "Data not update"}
 
 
+@router.delete('/delete/{todo_id}/')
+async def delete_todo(todo_id: int, session: Annotated[AsyncSession, Depends(get_session)]):
+    try:
+        await delete_data_from_db(session, todo_id)
+        return {"msg": "Data delete"}
+    except:
+        return {"msg error": "Data not delete"}
 
--- app/services/todolist.py
+++ app/services/todolist.py
@@ -1,18 +1,32 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import update, delete
 
 from models.model import ToDoListOrm
 from schemas.schemas import ToDoListPostDTO
 
+
+async def insert_data_to_db(session: AsyncSession, data: ToDoListPostDTO):
+    result = ToDoListOrm(**data.model_dump())
+    session.add(result)
+    await session.commit()
 
 
 async def select_todo_by_id(todo_id: int, session: AsyncSession):
     result = await session.get(ToDoListOrm, todo_id)
     return result
 
+
 async def update_todo_with_put(
-        todo_id: int,
-        session: AsyncSession,
-        data: ToDoListPostDTO
+    todo_id: int, session: AsyncSession, data: ToDoListPostDTO
 ):
-    todo = await session.get(ToDoListOrm, todo_id)
-    # todo.
+    stmt = update(ToDoListOrm).filter_by(id=todo_id).values(data.model_dump())
+    await session.execute(stmt)
+    await session.commit()
+
+
+async def delete_data_from_db(session: AsyncSession, todo_id: int):
+    stmt = (
+        delete(ToDoListOrm).filter_by(id=todo_id)
+    )
+    await session.execute(stmt)
+    await session.commit()
